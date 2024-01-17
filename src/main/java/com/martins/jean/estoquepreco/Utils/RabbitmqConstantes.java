3c0e1fbcd4af64165253b97df1b42ae178331b2f
jeanverso <jeanmarcel8@gmail.com>
2024-01-17 20:10:20 -0300

    criando projeto spring

---
+++ src/main/java/com/martins/jean/estoquepreco/Utils/RabbitmqConstantes.java
@@ -0,0 +1,7 @@
+package com.martins.jean.estoquepreco.Utils;
+
+public class RabbitmqConstantes {
+    public static final String FILA_ESTOQUE = "ESTOQUE";
+    public static final String FILA_PRECO = "PRECO";
+
+}
